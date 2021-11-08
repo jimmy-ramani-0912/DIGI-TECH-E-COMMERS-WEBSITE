eeff1f89b291f0a3de7023222d5ae2e9b3b66ab2
20IT120-JIMMY <80442931+20IT120-JIMMY@users.noreply.github.com>
2021-11-08 17:35:10 +0530

    Delete NEW ONE ADDED directory

--- NEW ONE ADDED/js/app.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.dropdown > a').forEach(e => {
-    e.addEventListener('click', (event) => event.preventDefault())
-})
-
-document.querySelectorAll('.mega-dropdown > a').forEach(e => {
-    e.addEventListener('click', (event) => event.preventDefault())
-})
